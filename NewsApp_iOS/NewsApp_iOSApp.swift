46e4bae9d5a663df523b6f4cf1da3108bf4df095
Ayushi Jethva <gohelayushi@gmail.com>
2025-02-22 20:10:37 -0500

    Add files via upload

---
+++ NewsApp_iOS/NewsApp_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp_iOSApp.swift
+//  NewsApp_iOS
+//
+//  Created by Ayushi Gohel on 2025-02-22.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
